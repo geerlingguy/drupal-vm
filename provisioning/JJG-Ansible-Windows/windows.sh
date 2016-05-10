9749414255ba42782bca4d38165cccd2c4d99e08
Jeff Geerling <geerlingguy@mac.com>
2016-05-10 08:16:03 -0500

    Fixes #594: Update JJG-Ansible-Windows to 1.3.0.

--- provisioning/JJG-Ansible-Windows/windows.sh
+++ provisioning/JJG-Ansible-Windows/windows.sh
@@ -72,7 +72,7 @@ fi
 
 # Install requirements.
 echo "Installing Ansible roles from requirements file, if available."
-find "/vagrant/$PLAYBOOK_DIR" \( -name "requirements.yml" -o -name "requirements.txt" \) -exec sudo ansible-galaxy install --ignore-errors -r {} \;
+find "/vagrant/$PLAYBOOK_DIR" \( -name "requirements.yml" -o -name "requirements.txt" \) -exec sudo ansible-galaxy install --force --ignore-errors -r {} \;
 
 # Run the playbook.
 echo "Running Ansible provisioner defined in Vagrantfile."
