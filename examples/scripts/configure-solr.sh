8816614be8b41c67a6e628d15701cb9ef22e9298
Jimmy Ko <egkcyaaa@gmail.com>
2017-01-20 16:09:09 +0000

    Create new solr collection instead of using the default one.

--- examples/scripts/configure-solr.sh
+++ examples/scripts/configure-solr.sh
@@ -7,14 +7,15 @@
 # `solr` in the `installed_extras`, and is currently set up for the D7 versions
 # of Apache Solr Search or Search API Solr.
 
-SOLR_SETUP_COMPLETE_FILE=/etc/drupal_vm_solr_config_complete
+NAME="drupalvm"
+SOLR_SETUP_COMPLETE_FILE="/etc/drupal_vm_solr_config_complete_"$NAME
 
 # Search API Solr module.
 SOLR_DOWNLOAD="http://ftp.drupal.org/files/projects/search_api_solr-8.x-1.x-dev.tar.gz"
 SOLR_DOWNLOAD_DIR="/tmp"
 SOLR_MODULE_NAME="search_api_solr"
 SOLR_VERSION="5.x"
-SOLR_CORE_PATH="/var/solr/data/collection1"
+SOLR_CORE_PATH="/var/solr/data/"$NAME
 
 # Check to see if we've already performed this setup.
 if [ ! -e "$SOLR_SETUP_COMPLETE_FILE" ]; then
@@ -22,13 +23,10 @@ if [ ! -e "$SOLR_SETUP_COMPLETE_FILE" ]; then
   wget -qO- $SOLR_DOWNLOAD | tar xvz -C $SOLR_DOWNLOAD_DIR
 
   # Copy the Solr configuration into place over the default `collection1` core.
-  sudo cp -a $SOLR_DOWNLOAD_DIR/$SOLR_MODULE_NAME/solr-conf/$SOLR_VERSION/. $SOLR_CORE_PATH/conf/
+  sudo su - solr -c "/opt/solr/bin/solr create -c $NAME -d $SOLR_DOWNLOAD_DIR/$SOLR_MODULE_NAME/solr-conf/$SOLR_VERSION/"
 
   # Adjust the autoCommit time so index changes are committed in 1s.
   sudo sed -i 's/\(<maxTime>\)\([^<]*\)\(<[^>]*\)/\11000\3/g' $SOLR_CORE_PATH/conf/solrconfig.xml
-
-  # Fix file permissions.
-  sudo chown -R solr:solr $SOLR_CORE_PATH/conf
 
   # Restart Apache Solr.
   sudo service solr restart
