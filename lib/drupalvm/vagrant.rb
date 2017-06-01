a9d80ddedbd124e25d76e5f6b6e611c1c4faa663
Oskar Schöldström <public@oxy.fi>
2017-06-01 11:10:32 -0500

    fix rubocop lint

--- lib/drupalvm/vagrant.rb
+++ lib/drupalvm/vagrant.rb
@@ -80,23 +80,23 @@ def ensure_plugins(plugins)
     plugin_name = plugin['name']
     manager = Vagrant::Plugin::Manager.instance
 
-    next if manager.installed_plugins.has_key?(plugin_name)
+    next if manager.installed_plugins.key?(plugin_name)
 
     logger.warn("Installing plugin #{plugin_name}")
 
     manager.install_plugin(
       plugin_name,
-      sources: plugin.fetch('source', %w(https://rubygems.org/ https://gems.hashicorp.com/)),
+      sources: plugin.fetch('source', %w[https://rubygems.org/ https://gems.hashicorp.com/]),
       version: plugin['version']
     )
 
     installed = true
   end
 
-  if installed
-    logger.warn('`vagrant up` must be re-run now that plugins are installed')
-    exit
-  end
+  return unless installed
+
+  logger.warn('`vagrant up` must be re-run now that plugins are installed')
+  exit
 end
 
 def get_apache_vhosts(vhosts)
